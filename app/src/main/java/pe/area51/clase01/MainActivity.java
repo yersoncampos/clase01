878943456ade7d7b1b6340540a9c6a70c157d304
Alumno <alumno@MM-A51-01.local>
2019-01-15 21:40:42 -0500

    primera commit
    ;

---
+++ app/src/main/java/pe/area51/clase01/MainActivity.java
@@ -0,0 +1,13 @@
+package pe.area51.clase01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
